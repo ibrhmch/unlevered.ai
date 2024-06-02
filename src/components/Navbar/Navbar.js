d7b9e4196b3d1815d3a9b2ead73beff31d6c9944
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-02 15:47:30 -0600

    create Navbar component to render TopBar and BavLinks

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TopBar from './TopBar';
+import NavLinks from './NavLinks';
+
+function Navbar() {
+    return (
+        <div>
+            <TopBar />
+            <NavLinks />
+        </div>
+    );
+};
+
+export default Navbar;
