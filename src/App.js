547b2b0cc10a14226b590709ced6d690740961b5
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-02 16:24:20 -0600

    added default text styling and removed the use of arrow function for component decl

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import HomePage from './pages/HomePage';
 
-const App = () => {
+function App() {
   return (
-    <div>
+    <div className="App" style={{ backgroundColor: 'rgb(31 41 55)', minHeight: '100vh', color: 'white' }}>
       <Navbar />
+      <HomePage />
     </div>
   );
-};
+}
 
 export default App;
