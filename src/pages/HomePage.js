72407d41ac17f9ba6595d01c0e6ac549e404ffcb
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-02 16:23:37 -0600

    created a homepage component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Welcome to Unlevered</h1>
+            <p className="mt-4">This is the homepage. Use the navigation bar to browse different sections.</p>
+        </div>
+    );
+};
+
+export default HomePage;
