b44643a08bb8017238be8704184b07cf53c5b4cf
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-03 01:59:58 -0600

    to display stock info and chart

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,59 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import StockOverview from "../components/StockOverview/StockOverview";
+import CandlestickChart from "../components/CandlestickChart/CandlestickChart";
+
 function HomePage() {
+    const [stockFrequency, setStockFrequency] = useState('WEEKLY');
+    const [stockName, setStockName] = useState('AAPL');
+
+    const changeStockFrequency = (frequency) => {
+        setStockFrequency(frequency);
+    };
+
+    // Dummy data for now
+    const dummyStockData = {
+        name: 'Apple Inc.',
+        price: 145.09,
+        change: -1.23,
+        changePercent: -0.84
+    };
+
     return (
-        <div>
-            <h1 className="text-2xl font-bold">Welcome to Unlevered</h1>
-            <p className="mt-4">This is the homepage. Use the navigation bar to browse different sections.</p>
+        <div className="App min-h-screen text-gray-200">
+            <StockOverview
+                name={dummyStockData.name}
+                price={dummyStockData.price}
+                change={dummyStockData.change}
+                changePercent={dummyStockData.changePercent}
+            />
+            <div className="pt-2 pb-2">
+                <div className="flex justify-center mb-6">
+                    <button
+                        className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mx-2"
+                        onClick={() => changeStockFrequency('DAILY')}
+                    >
+                        Daily
+                    </button>
+                    <button
+                        className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mx-2"
+                        onClick={() => changeStockFrequency('WEEKLY')}
+                    >
+                        Weekly
+                    </button>
+                    <button
+                        className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mx-2"
+                        onClick={() => changeStockFrequency('MONTHLY')}
+                    >
+                        Monthly
+                    </button>
+                </div>
+                <div className="flex justify-center max-w-full">
+                    <CandlestickChart stockFrequency={stockFrequency} stockName={stockName} />
+                </div>
+            </div>
         </div>
     );
-};
+}
 
 export default HomePage;
