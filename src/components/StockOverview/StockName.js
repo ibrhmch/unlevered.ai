89dc38b55ff4baf683e85ab9e5d485d35851220a
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-03 01:15:40 -0600

    created to show stock name

---
+++ src/components/StockOverview/StockName.js
@@ -0,0 +1,9 @@
+function StockName({ name }) {
+    return (
+        <div className="flex-1">
+            <h1 className="text-3xl font-bold">{name}</h1>
+        </div>
+    );
+}
+
+export default StockName;
