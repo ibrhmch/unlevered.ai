0ca334ea417344454434208098f3cc56dfc36f78
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-04 16:22:31 -0600

    added logic to change cell value text colors based on qualifying keys

--- src/components/Tables/KeyRatios.js
+++ src/components/Tables/KeyRatios.js
@@ -1,6 +1,32 @@
+import React from 'react';
+
 function KeyRatios({ stockKeyRatios }) {
     const keyStyling = 'px-2 p-1';
     const valueStyling = 'font-thin italic px-2 py-1';
+
+    const conditionalKeys = [
+        "Sales Surprise", "SMA20", "PEG", "P/B", "Debt/Eq", "LT Debt/Eq", "EPS Surprise",
+        "SMA50", "Sales Y/Y TTM", "Sales Q/Q", "SMA200", "ROA", "ROE", "ROI",
+        "Oper. Margin", "Profit Margin", "52W High", "52W Low", "Perf Week",
+        "Perf Month", "Perf Quarter", "Perf Half Y", "Perf Year", "Perf YTD",
+        "Target Price", "Change"
+    ];
+
+    const getClassNameForValue = (key, value) => {
+        if (conditionalKeys.includes(key)) {
+            let numericValue;
+            if (typeof value === 'string' && value.includes('%')) {
+                numericValue = parseFloat(value.replace('%', ''));
+            } else {
+                numericValue = parseFloat(value);
+            }
+
+            if (!isNaN(numericValue)) {
+                return numericValue >= 0 ? 'text-unlevered-green' : 'text-unlevered-red';
+            }
+        }
+        return '';
+    };
 
     const renderTableRows = () => {
         const entries = Object.entries(stockKeyRatios);
@@ -12,37 +38,37 @@ function KeyRatios({ stockKeyRatios }) {
                     {entries[i] && (
                         <>
                             <td className={`${keyStyling}`}>{entries[i][0]}</td>
-                            <td className={`${valueStyling}`}>{entries[i][1]}</td>
+                            <td className={`${valueStyling} ${getClassNameForValue(entries[i][0], entries[i][1])}`}>{entries[i][1]}</td>
                         </>
                     )}
                     {entries[i + 1] && (
                         <>
                             <td className={`${keyStyling}`}>{entries[i + 1][0]}</td>
-                            <td className={`${valueStyling}`}>{entries[i + 1][1]}</td>
+                            <td className={`${valueStyling} ${getClassNameForValue(entries[i + 1][0], entries[i + 1][1])}`}>{entries[i + 1][1]}</td>
                         </>
                     )}
                     {entries[i + 2] && (
                         <>
                             <td className={`${keyStyling}`}>{entries[i + 2][0]}</td>
-                            <td className={`${valueStyling}`}>{entries[i + 2][1]}</td>
+                            <td className={`${valueStyling} ${getClassNameForValue(entries[i + 2][0], entries[i + 2][1])}`}>{entries[i + 2][1]}</td>
                         </>
                     )}
                     {entries[i + 3] && (
                         <>
                             <td className={`${keyStyling}`}>{entries[i + 3][0]}</td>
-                            <td className={`${valueStyling}`}>{entries[i + 3][1]}</td>
+                            <td className={`${valueStyling} ${getClassNameForValue(entries[i + 3][0], entries[i + 3][1])}`}>{entries[i + 3][1]}</td>
                         </>
                     )}
                     {entries[i + 4] && (
                         <>
                             <td className={`${keyStyling}`}>{entries[i + 4][0]}</td>
-                            <td className={`${valueStyling}`}>{entries[i + 4][1]}</td>
+                            <td className={`${valueStyling} ${getClassNameForValue(entries[i + 4][0], entries[i + 4][1])}`}>{entries[i + 4][1]}</td>
                         </>
                     )}
                     {entries[i + 5] && (
                         <>
                             <td className={`${keyStyling}`}>{entries[i + 5][0]}</td>
-                            <td className={`${valueStyling}`}>{entries[i + 5][1]}</td>
+                            <td className={`${valueStyling} ${getClassNameForValue(entries[i + 5][0], entries[i + 5][1])}`}>{entries[i + 5][1]}</td>
                         </>
                     )}
                 </tr>
