72aaa14a8fbb76c17ab8606d4c82ef2f6bf07003
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-03 23:21:11 -0600

    updated styling for frequency buttons

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -20,29 +20,29 @@ function HomePage() {
     };
 
     return (
-        <div className="App min-h-screen text-gray-200 mt-20 w-full">
+        <div className="App min-h-screen text-gray-300 font-light mt-20 w-full">
             <StockOverview
                 name={dummyStockData.name}
                 price={dummyStockData.price}
                 change={dummyStockData.change}
                 changePercent={dummyStockData.changePercent}
             />
-            <div className="pt-2 pb-2">
+            <div className="pt-2 pb-2 text-sm">
                 <div className="flex justify-center mb-6">
                     <button
-                        className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mx-2"
+                        className={`hover:bg-gray-800 py-2 px-4 mx-2 rounded-md ${stockFrequency === 'DAILY' ? 'bg-gray-900 border border-unlevered-textpurple' : ''}`}
                         onClick={() => changeStockFrequency('DAILY')}
                     >
                         Daily
                     </button>
                     <button
-                        className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mx-2"
+                        className={`hover:bg-gray-800 py-2 px-4 mx-2 rounded-md ${stockFrequency === 'WEEKLY' ? 'bg-gray-900 border border-unlevered-textpurple' : ''}`}
                         onClick={() => changeStockFrequency('WEEKLY')}
                     >
                         Weekly
                     </button>
                     <button
-                        className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mx-2"
+                        className={`hover:bg-gray-800 py-2 px-4 mx-2 rounded-md ${stockFrequency === 'MONTHLY' ? 'bg-gray-900 border border-unlevered-textpurple' : ''}`}
                         onClick={() => changeStockFrequency('MONTHLY')}
                     >
                         Monthly
