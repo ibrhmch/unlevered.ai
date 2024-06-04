c01463e0efc3e5d1aaa069881548e907795a7b03
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-04 14:00:07 -0600

    displaying the new stats table and moved buttons logic out to a new component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,14 +2,38 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import StockOverview from "../components/StockOverview/StockOverview";
 import CandlestickChart from "../components/CandlestickChart/CandlestickChart";
+import FrequencyButtons from "../components/CandlestickChart/FrequencyButtons";
+import StatsTable from "../components/StatsTables/StatsTable";
+
 
 function HomePage() {
     const [stockFrequency, setStockFrequency] = useState('WEEKLY');
     const [stockName, setStockName] = useState('AAPL');
+    const [stockFinacialReports, setStockFinancialReports] = useState({});
+    const [stockForecasts, setStockForecasts] = useState([]);
 
     const changeStockFrequency = (frequency) => {
         setStockFrequency(frequency);
     };
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/financials')
+            .then((response) => {
+                setStockFinancialReports(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data: ', error);
+            });
+
+        axios.get('http://localhost:3001/forecasts')
+            .then((response) => {
+                setStockForecasts(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data: ', error);
+            });
+
+    }, [stockFrequency]);
 
     // Dummy data for now
     const dummyStockData = {
@@ -20,7 +44,7 @@ function HomePage() {
     };
 
     return (
-        <div className="App min-h-screen text-gray-300 font-light mt-20 w-full">
+        <div className="min-h-screen text-gray-300 font-light mt-20 w-full">
             <StockOverview
                 name={dummyStockData.name}
                 price={dummyStockData.price}
@@ -28,30 +52,23 @@ function HomePage() {
                 changePercent={dummyStockData.changePercent}
             />
             <div className="pt-2 pb-2 flex justify-center text-sm">
-                <div className="flex flex-col py-4 w-full mx-1 lg:w-4/6 rounded-md border border-gray-800">
+                <div className="flex flex-col py-4 w-full mx-1 lg:w-4/6 rounded-md border-zinc-600 border bg-graph-background">
                     <div className="flex justify-center mb-6">
-                        <button
-                            className={`hover:bg-gray-800 py-2 px-4 mx-2 rounded-md ${stockFrequency === 'DAILY' ? 'bg-gray-900 border border-unlevered-textpurple' : ''}`}
-                            onClick={() => changeStockFrequency('DAILY')}
-                        >
-                            Daily
-                        </button>
-                        <button
-                            className={`hover:bg-gray-800 py-2 px-4 mx-2 rounded-md ${stockFrequency === 'WEEKLY' ? 'bg-gray-900 border border-unlevered-textpurple' : ''}`}
-                            onClick={() => changeStockFrequency('WEEKLY')}
-                        >
-                            Weekly
-                        </button>
-                        <button
-                            className={`hover:bg-gray-800 py-2 px-4 mx-2 rounded-md ${stockFrequency === 'MONTHLY' ? 'bg-gray-900 border border-unlevered-textpurple' : ''}`}
-                            onClick={() => changeStockFrequency('MONTHLY')}
-                        >
-                            Monthly
-                        </button>
+                        <FrequencyButtons stockFrequency={stockFrequency} changeStockFrequency={changeStockFrequency} />
                     </div>
                     <div className="flex justify-center">
                         <CandlestickChart stockFrequency={stockFrequency} stockName={stockName} />
                     </div>
+                </div>
+            </div>
+            <div className="pt-2 pb-2 flex justify-center text-sm">
+                <div className="w-full lg:w-4/6 flex justify-center items-center">
+                    <StatsTable stockFinacialReports={stockFinacialReports} />
+                </div>
+            </div>
+            <div className="pt-2 pb-2 flex justify-center text-sm bg-slate-500">
+                <div className="bg-green-500 w-full lg:w-4/6 flex justify-center items-center">
+                    <pre>{JSON.stringify(stockForecasts, null, 2)}</pre>
                 </div>
             </div>
         </div>
