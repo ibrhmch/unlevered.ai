42dd04676040e5244fd97d7cfc051f45d3e9e733
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-03 22:20:53 -0600

    removed background color styling classg

--- src/components/StockOverview/StockOverview.js
+++ src/components/StockOverview/StockOverview.js
@@ -3,7 +3,7 @@ import StockQuickInfo from './StockQuickInfo';
 
 function StockOverview({ name, price, change, changePercent }) {
     return (
-        <div className="flex justify-between items-center bg-indigo-950 p-6 my-2">
+        <div className="flex justify-between items-center p-6 my-2">
             <StockName name={name} />
             <StockQuickInfo price={price} change={change} changePercent={changePercent} />
         </div>
