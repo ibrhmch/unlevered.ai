353a46b3f440c43932c5b1fb05feeb97dceddc5a
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-03 22:52:54 -0600

    added unlevered green and red colors for graphs

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
       colors: {
         'unleveredpurple': '#070415',
         'unleveredpurple-light': '#2b0f51',
-        'unlevered-textpurple': '#948CF2'
+        'unlevered-textpurple': '#948CF2',
+        'unlevered-green': '#00FFC0',
+        'unlevered-red': '#ff5959',
       },
     },
   },
