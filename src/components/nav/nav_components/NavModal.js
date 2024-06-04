ce71372d04d1bd8bfc32f9f6dc71a8ae266945f5
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-03 22:51:06 -0600

    final styling changes and corrected contact info

--- src/components/nav/nav_components/NavModal.js
+++ src/components/nav/nav_components/NavModal.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import "./NavModal.css";
 
 const icons = ['fas fa-mobile-alt', 'far fa-envelope', 'fas fa-map-pin'];
-const contacts = ['212 444 8786', 'Tower@towerce.com', '120 West 31st Street, 7th Floor, New York, New York'];
+const contacts = ['212 xxx xxxx', 'contact@unlevered.ai', 'Lower Manhattan, New York'];
 
 function NavModal({ isModalOpen, onClose }) {
     useEffect(() => {
@@ -24,40 +24,32 @@ function NavModal({ isModalOpen, onClose }) {
 
     return (
         <div className=" font-thin fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-            <div className="bg-white rounded-md w-4/5 md:w-full max-w-md pb-3">
+            <div className="bg-gray-600 rounded-md w-4/5 md:w-full max-w-md pb-3">
                 <div className="flex flex-grow flex-row">
                     <h2 className="py-2 ml-10">Business Hours | <i>9 am to 5 pm</i></h2>
-                    <button className="m-1 px-2 py-1 hover:bg-gray-100 rounded-md ml-auto" onClick={onClose}><i className="fas fa-times"></i></button>
+                    <button className="m-1 px-2 py-1 hover:bg-unleveredpurple rounded-md ml-auto" onClick={onClose}><i className="fas fa-times"></i></button>
                 </div>
                 <div className="flex flex-col mt-5 px-10">
                     <div className="flex flex-row justify-start items-center">
                         <div className="flex flex-row justify-center mb-2 px-2 w-1">
                             <i className="fas fa-mobile-alt"></i>
                         </div>
-                        <p className="mb-2 ml-2 py-1 px-3 transition-colors duration-300 ease-out hover:bg-gray-100"><a href="tel:+12124448786">212 444 8786</a></p>
+                        <p className="mb-2 ml-2 py-1 px-3 transition-colors duration-300 ease-out hover:bg-unleveredpurple rounded-md"><a href="tel:+1212xxxxxxx">212 xxx xxxx</a></p>
                     </div>
 
                     <div className="flex flex-row justify-start items-center">
                         <div className="flex flex-row justify-center mb-2 px-2 w-1">
                             <i className="far fa-envelope"></i>
                         </div>
-                        <p className="mb-2 ml-2 py-1 px-3 transition-colors duration-300 ease-out hover:bg-gray-100"><a href="mailto:Info@towerce.com" className="">Info@towerce.com</a></p>
+                        <p className="mb-2 ml-2 py-1 px-3 transition-colors duration-300 ease-out hover:bg-unleveredpurple rounded-md"><a href="mailto:contact@unlevered.ai" className="">contact@unlevered.ai</a></p>
                     </div>
 
                     <div className="flex flex-row justify-start items-center">
                         <div className="flex flex-row justify-center mb-2 px-2 w-1">
                             <i className="fas fa-map-pin"></i>
                         </div>
-                        <p className="mb-2 ml-2 py-1 px-3 transition-colors duration-300 ease-out hover:bg-gray-100"><a target="_blank" href="https://maps.app.goo.gl/HCFZV9LrjuNtexoo9">120 West 31st Street, 7th Floor, New York, New York</a></p>
+                        <p className="mb-2 ml-2 py-1 px-3 transition-colors duration-300 ease-out hover:bg-unleveredpurple rounded-md"><a target="_blank" href="https://maps.app.goo.gl/4uPZuHAdknmJV77w8">Lower Manhattan, New York</a></p>
                     </div>
-                    {/* <div className="flex justify-center p-4 font-extralight">
-                        <div className="" dangerouslySetInnerHTML={
-                            {
-                                __html: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.6313061653036!2d-73.99291048748276!3d40.74813763528156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259af3d4aa9fd%3A0x90059aaabd063cc2!2s120%20W%2031st%20St%2C%20New%20York%2C%20NY%2010001!5e0!3m2!1sen!2sus!4v1700954922139!5m2!1sen!2sus" width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
-                            }
-                        } />
-                    </div> */}
-
                 </div>
             </div>
         </div>
