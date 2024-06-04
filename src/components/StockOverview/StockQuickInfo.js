0a83bc9e67a1d5233c59a03b7bd7c825e0235ff8
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-03 22:54:56 -0600

    using unlevered colors

--- src/components/StockOverview/StockQuickInfo.js
+++ src/components/StockOverview/StockQuickInfo.js
@@ -1,8 +1,8 @@
 function StockQuickInfo({ price, change, changePercent }) {
     return (
-        <div className="flex-1 text-right">
-            <p className="text-xl">Price: ${price}</p>
-            <p className={`text-xl ${change >= 0 ? 'text-green-500' : 'text-red-500'}`}>
+        <div className="flex-1 text-right font-medium">
+            <p className="text-lg">Price: ${price}</p>
+            <p className={`text-xl ${change >= 0 ? 'text-unlevered-green' : 'text-unlevered-red'}`}>
                 {change >= 0 ? '+' : ''}{change} ({changePercent}%)
             </p>
         </div>
