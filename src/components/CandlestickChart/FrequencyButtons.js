888a6e1ab28d90bc7b921bce7b20ec6552bc899a
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-04 19:19:44 -0600

    changed frequency buttons vertical padding

--- src/components/CandlestickChart/FrequencyButtons.js
+++ src/components/CandlestickChart/FrequencyButtons.js
@@ -2,19 +2,19 @@ function FrequencyButtons({ stockFrequency, changeStockFrequency }) {
     return (
         <>
             <button
-                className={`hover:bg-gray-800 py-2 px-4 mx-2 rounded-md ${stockFrequency === 'DAILY' ? 'bg-gray-900 border border-unlevered-textpurple' : ''}`}
+                className={`hover:bg-gray-800 py-1 px-4 mx-2 rounded-md ${stockFrequency === 'DAILY' ? 'bg-gray-900 border border-unlevered-textpurple' : ''}`}
                 onClick={() => changeStockFrequency('DAILY')}
             >
                 Daily
             </button>
             <button
-                className={`hover:bg-gray-800 py-2 px-4 mx-2 rounded-md ${stockFrequency === 'WEEKLY' ? 'bg-gray-900 border border-unlevered-textpurple' : ''}`}
+                className={`hover:bg-gray-800 py-1 px-4 mx-2 rounded-md ${stockFrequency === 'WEEKLY' ? 'bg-gray-900 border border-unlevered-textpurple' : ''}`}
                 onClick={() => changeStockFrequency('WEEKLY')}
             >
                 Weekly
             </button>
             <button
-                className={`hover:bg-gray-800 py-2 px-4 mx-2 rounded-md ${stockFrequency === 'MONTHLY' ? 'bg-gray-900 border border-unlevered-textpurple' : ''}`}
+                className={`hover:bg-gray-800 py-1 px-4 mx-2 rounded-md ${stockFrequency === 'MONTHLY' ? 'bg-gray-900 border border-unlevered-textpurple' : ''}`}
                 onClick={() => changeStockFrequency('MONTHLY')}
             >
                 Monthly
